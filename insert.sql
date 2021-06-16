9c90799171e8d3f59b244f607341f5eed0191b94
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-06-16 19:07:38 +0400

    modified insert

--- insert.sql
+++ insert.sql
@@ -3,7 +3,7 @@ VALUES
 ('PHI','INDIA'),
 ('Harper','USA'),
 ('GCP','USA'),
-('Atomic Habits','USA'),
+('Avery','USA'),
 ('Del Rey','UK'),
 ('Vintage','UK');
 
@@ -32,37 +32,27 @@ VALUES
 ('The Great War for Civilisation',6);
 
 INSERT INTO books_subjects(book, subject)
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The C Programming Language' AND subjects.name='C'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The C Programming Language' AND subjects.name='UNIX'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The C Programming Language' AND subjects.name='Technology'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The Go Programming Language' AND subjects.name='Go'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The Go Programming Language' AND subjects.name='Technology'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The UNIX Programming Environment' AND subjects.name='UNIX'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The UNIX Programming Environment' AND subjects.name='Technology'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Cryptonomicon' AND subjects.name='Technology'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Cryptonomicon' AND subjects.name='Science Fiction'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Deep Work' AND subjects.name='Productivity'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Deep Work' AND subjects.name='Technology'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Atomic Habits' AND subjects.name='Psychology'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Atomic Habits' AND subjects.name='Productivity'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The City and The City' AND subjects.name='Science Fiction'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The City and The City' AND subjects.name='Politics'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The Great War for Civilisation' AND subjects.name='History'
-UNION
-SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The Great War for Civilisation' AND subjects.name='Politics'
-;
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The C Programming Language' AND subjects.name='C' UNION
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The C Programming Language' AND subjects.name='UNIX' UNION
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The C Programming Language' AND subjects.name='Technology' UNION
+
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The Go Programming Language' AND subjects.name='Go' UNION
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The Go Programming Language' AND subjects.name='Technology' UNION
+
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The UNIX Programming Environment' AND subjects.name='UNIX' UNION
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The UNIX Programming Environment' AND subjects.name='Technology' UNION
+
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Cryptonomicon' AND subjects.name='Technology' UNION
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Cryptonomicon' AND subjects.name='Science Fiction' UNION
+
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Deep Work' AND subjects.name='Productivity'UNION
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Deep Work' AND subjects.name='Technology' UNION
+
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Atomic Habits' AND subjects.name='Psychology'UNION
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='Atomic Habits' AND subjects.name='Productivity'UNION
+
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The City and The City' AND subjects.name='Science Fiction'UNION
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The City and The City' AND subjects.name='Politics'UNION
+
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The Great War for Civilisation' AND subjects.name='History'UNION
+SELECT books.id,subjects.id FROM books,subjects WHERE books.title='The Great War for Civilisation' AND subjects.name='Politics';
